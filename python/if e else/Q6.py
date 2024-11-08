b75287f3093a543b44dd5a915ff0e31fc0922350
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-11-08 16:42:50 +0000

    fe

--- python/if e else/Q6.py
+++ python/if e else/Q6.py
@@ -7,9 +7,12 @@ Se sim, determine se é um triângulo equilátero, isósceles ou escaleno.
 l1=float(input("Digite o tomanho do lado 1: "))
 l2=float(input("Digite o tomanho do lado 2: "))
 l3=float(input("Digite o tomanho do lado 3: "))
-if l1 == l2 and l2 == l3:
-    print("O triangulo é Equilátero")
-elif (l1 == l2) or (l2 == l3) or (l3 == l1):
-    print("O triangulo é Isósceles")
+if (l1 >= l2 + l3) or (l2 >= l1 + l3) or (l3 >= l2 + l1):
+    print("Não é um triangulo valido")
 else:
-    print("O triangulo é escaleno")
+    if l1 == l2 and l2 == l3:
+        print("O triangulo é Equilátero")
+    elif (l1 == l2) or (l2 == l3) or (l3 == l1):
+        print("O triangulo é Isósceles")
+    else:
+        print("O triangulo é escaleno")
