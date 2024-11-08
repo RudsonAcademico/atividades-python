8f63b232c7eb22b76b5bc5bcafea686f255b30e7
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-11-08 17:19:06 +0000

    yio

---
+++ python/if e else/Q10.py
@@ -0,0 +1,12 @@
+#Verificação de Ano Bissexto: Escreva um programa que verifica se um ano fornecido pelo usuário é bissexto ou não.
+ano=int(input("Digite um ano: "))
+if ano%4==0:
+    if ano%100==0:
+        if ano%400==0:
+            print("é bissexto")
+        else:
+            print("não é bissexto")
+    else:
+        print("é bissexto")
+else:
+    print("não é bissexto")
