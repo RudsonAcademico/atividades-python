96d4d2a7392aca33f75540372696dccace2fb93c
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-11-08 17:34:50 +0000

    utes

--- python/if e else/Q10.py
+++ python/if e else/Q10.py
@@ -10,3 +10,8 @@ if ano%4==0:
         print("é bissexto")
 else:
     print("não é bissexto")
+
+if (ano%400==0) or ((ano%4==0) and (ano%100!=0)):
+    print("Bissexto")
+else:
+    print("Não bissexto")
