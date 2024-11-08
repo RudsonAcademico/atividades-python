ece0141f8074b7842a41ab7cde30fbb2197c6673
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-11-08 16:59:37 +0000

    w

--- python/loops/Q4.py
+++ python/loops/Q4.py
@@ -1,5 +1,5 @@
 #Escreva um programa que utilize um loop "for" para calcular a soma de todos os números ímpares de 1 a 100.
 n=0
-for x in range(0,101,2):
+for x in range(1,101,2):
     n+=x
 print(n)
