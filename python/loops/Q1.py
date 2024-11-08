32fbb816d6cf62aa4f983b082caf430676832c39
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-11-08 16:59:05 +0000

    dw

--- python/loops/Q1.py
+++ python/loops/Q1.py
@@ -1,4 +1,3 @@
 #Escreva um programa que imprima todos os números pares de 0 a 50 utilizando um loop "for".
-for x in range(51):
-    if x%2==0:
-        print(x)
+for x in range(0,51,2):
+    print(x)
--- python/loops/Q4.py
+++ python/loops/Q4.py
@@ -1,6 +1,5 @@
 #Escreva um programa que utilize um loop "for" para calcular a soma de todos os números ímpares de 1 a 100.
 n=0
-for x in range(101):
-    if x%2 != 0:
-        n+=x
+for x in range(0,101,2):
+    n+=x
 print(n)
