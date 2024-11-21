5a100a957a1abb74609f6bea3d6510a27e2d974e
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-11-21 19:34:12 +0000

    fwvaev

--- python/f(string)/Q12.py
+++ python/f(string)/Q12.py
@@ -1,3 +1,3 @@
 #Escreva um programa que solicite ao usuário uma frase e imprima uma mensagem formatada mostrando a quantidade de caracteres, palavras e linhas na frase.
 f=input("Digite uma frase: ")
-print(f"{f} tem {len(f)} caracteres e {len(f.split())} linhas")
+print(f"{f} tem {len(f)} caracteres, {len(f.split())} palavras e {len(f.split("\n"))} linhas")
