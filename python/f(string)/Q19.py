fae5c295ef209451d44dee06168296add02b5f8b
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-11-21 20:26:10 +0000

    csz

--- python/f(string)/Q19.py
+++ python/f(string)/Q19.py
@@ -1,9 +1,4 @@
 #Escreva um programa que solicite ao usuário para digitar uma frase e verifique se ela contém a palavra "Python"
-f=input("Digite uma frase: ").split()
-for i in range(len(f)):
-    if f[i].lower() == "python":
-        x="Tem Python"
-        break
-    else:
-        x="Não Tem Python"
-print(x)
+f=input("Digite uma frase: ").lower()
+p= "Tem Python" if f.count('python') >= 1 else "Não tem Python"
+print(p)
