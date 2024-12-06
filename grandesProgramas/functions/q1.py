709588e4ce4844d48e98ffeabae3b23a7ba68b20
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-12-06 17:54:17 +0000

    hea

--- grandesProgramas/functions/q1.py
+++ grandesProgramas/functions/q1.py
@@ -4,7 +4,7 @@ def saudacao(nome:str) -> None:
     return f"Olá, {nome}!"
 
 
-#Crie uma função dobro que aceita um número como argumento e retorna o dobro desse número.
+#Crie uma função dobro que aceita um número como argumento e retorna o dobro desse número
 def dobro(numero:int) -> int:
     numero*=2
     return numero
