10c9bad8c1d73fba99a24712ef36d0f870eea16b
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-12-06 17:53:13 +0000

    mais um

--- grandesProgramas/functions/q1.py
+++ grandesProgramas/functions/q1.py
@@ -1,6 +1,34 @@
 #Escreva uma função chamada saudacao que aceita um nome como argumento e imprime "Olá, [nome]!".
 
 def saudacao(nome:str) -> None:
-    print(f"Olá, {nome}")
+    return f"Olá, {nome}!"
 
-saudacao("Rúdson Alisson")
+
+#Crie uma função dobro que aceita um número como argumento e retorna o dobro desse número.
+def dobro(numero:int) -> int:
+    numero*=2
+    return numero
+
+
+"""
+Crie uma função chamada saudacao_personalizada que aceita um nome e um idioma como argumentos.
+O idioma é opcional e padrão para "inglês". A função deve retornar uma saudação no idioma especificado.
+"""
+def saudacao_personalizada(nome:str, idioma:str) -> str:
+    """
+    Uma função que aceita um nome e um idioma como argumentos.
+    O idioma é opcional e padrão para "inglês". A função deve retornar uma saudação no idioma especificado.
+    """
+    match idioma:
+        case "japonês" | '1':
+            return(f"Kon'nichiwa {nome}-san!")
+        case "espanhol" | '2':
+            return(f"Hola, {nome}!")
+        case "grego" | '3':
+            return(f"Geia sou {nome}!")
+        case "francês" | '4':
+            return(f"Bonjour, {nome}!")
+        case "português" | '5':
+            return(f"Olá, {nome}!")
+        case _:
+            return(f"Hello, {nome}!")
--- grandesProgramas/functions/q2.py
+++
@@ -1,6 +0,0 @@
-#Crie uma função dobro que aceita um número como argumento e retorna o dobro desse número.
-def dobro(numero:int) -> int:
-    numero*=2
-    return(numero)
-numero=dobro(2)
-print(numero)
