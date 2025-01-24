4e7b8cc08625f8cce83496f95cd168700d0052d7
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2025-01-24 17:49:18 +0000

    dsad

--- grandesProgramas/functions/q1.py
+++ grandesProgramas/functions/q1.py
@@ -32,3 +32,37 @@ def saudacao_personalizada(nome:str, idioma:str) -> str:
             return(f"Olá, {nome}!")
         case _:
             return(f"Hello, {nome}!")
+
+#Escreva uma função potencia que aceita uma base e um expoente (com expoente padrão igual a 2) e retorna a base elevada ao expoente
+def potencia(base:int, expoente:int) -> int:
+    #Uma função potencia que aceita uma base e um expoente (com expoente padrão igual a 2) e retorna a base elevada ao expoente
+    if expoente == None:
+        return(base**2)
+    else:
+        return(base**expoente)
+
+"""
+Crie uma função chamada idade_valida que verifica se a idade fornecida como argumento é um número inteiro válido entre 0 e 150
+"""
+def idade_valida(idade:int) -> bool:
+    if type(idade) == int:
+        if idade >= 0 and idade <= 150:
+            return True
+        else: 
+            return False
+    else:
+        return False
+
+"""
+Implemente uma função validar_email que verifica se uma string fornecida como argumento representa um endereço de e-mail válido.
+Considere que um email válido não deve ter espaços, deve conter 01 arroba e terminar com .com
+"""
+def validar_email(email:str) -> bool:
+    arroba = True if email.count("@") == 1 else False
+    espaco = True if email.count(" ") != 0 else False
+    final = True if email[-4:] == ".com" else False
+    ponto = True if email.count("@.") != 0 else False
+    if arroba and espaco and final and ponto:
+        return True
+    else:
+        return False
